b2bf624ac68776ea04b82f14dc9e7670cd4f9d1b
RenatoSchipper <renattoschipper@gmail.com>
2024-07-29 16:10:31 -0300

    Espera fixa, implícita e explicita

--- src/main/java/TesteSincronismo.java
+++ src/main/java/TesteSincronismo.java
@@ -1,9 +1,15 @@
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TesteSincronismo {
 
@@ -24,9 +30,23 @@ public class TesteSincronismo {
 	}
 	
 	@Test
-	public void interagirComRespostaDemorada() throws InterruptedException {
+	public void utilizarEsperaFixa() throws InterruptedException {
 		dsl.clicarBotao("buttonDelay");
 		Thread.sleep(5000);
+		dsl.escrever("novoCampo", "Renato Schipper");
+	}
+	@Test
+	public void utilizarEsperaImplicita() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		dsl.clicarBotao("buttonDelay");
+		dsl.escrever("novoCampo", "Renato Schipper");
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+	}
+	@Test
+	public void utilizarEsperaExplicita() throws InterruptedException {
+		dsl.clicarBotao("buttonDelay");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("novoCampo")));
 		dsl.escrever("novoCampo", "Renato Schipper");
 	}
 }
